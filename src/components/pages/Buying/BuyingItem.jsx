c4746af2a0099540b4c806a3e7b58ccd8803e58a
Baxa-1998 <baxtiyornarziev6@gmail.com>
2024-12-15 16:02:56 +0300

    ss

--- src/components/pages/Buying/BuyingItem.jsx
+++ src/components/pages/Buying/BuyingItem.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import './buying.scss'
+import React from 'react';
+import './buying.scss';
 export default function BuyingItem() {
+  function test() {
+    console.log('click');
+  }
   return (
-    <div className='buying__item'> 
-        
-    <div className='buying__item_img'>
-      <img src="/elephant/1.png" alt="elephant" />
+    <div className="buying__item">
+      <div className="buying__item_img">
+        <img src="/elephant/1.png" alt="elephant" />
+      </div>
+      <div className="buying__item_info">
+        <div className="buying__lvl">
+          <img onClick={test} src="/arrow-left.png" alt="<" />
+          <span>Слон 1 lvl</span>
+          <img src="arrow-right.png" alt=">" />
+        </div>
+        <div className="buying__discount">
+          <h4>5%</h4>
+          <p>Вы зарабатываете с каждой <br /> продажи слона другу !</p>
+        </div>
+      </div>
     </div>
-    <div className='buying__item_info'>
-
-    </div>
-
-  </div>
-  )
+  );
 }
