04d166744f373348b9b0793297ee504359314b85
Baxa-1998 <baxtiyornarziev6@gmail.com>
2024-12-15 20:49:30 +0300

    ss

--- src/components/pages/Buying/Buying.jsx
+++ src/components/pages/Buying/Buying.jsx
@@ -8,9 +8,90 @@ import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 
 export default function Buying() {
-  const data = useState([{
-    
-  }])
+  const [data, setData] = useState([
+    {
+    id: 1,
+    level: 1,
+    discount: 5,
+    img: '/elephant/1.png',
+    stars: 150,
+    price: 100
+  },
+  {
+    id: 2,
+    level: 2,
+    discount: 10,
+    img: '/elephant/2.png',
+    stars: 300,
+    price: 200
+  },
+  {
+    id: 3,
+    level: 3,
+    discount: 15,
+    img: '/elephant/3.png',
+    stars: 450,
+    price: 300
+
+  },
+  {
+    id: 4,
+    level: 4,
+    discount: 20,
+    img: '/elephant/4.png',
+    stars: 600,
+    price: 400
+  },
+  {
+    id: 5,
+    level: 5,
+    discount: 25,
+    img: '/elephant/5.png',
+    stars: 750,
+    price: 500
+  },
+  {
+    id: 6,
+    level: 6,
+    discount: 30,
+    img: '/elephant/6.png',
+    stars: 900,
+    price: 600
+  },
+  {
+    id: 7,
+    level: 7,
+    discount: 35,
+    img: '/elephant/7.png',
+    stars: 1050,
+    price: 700
+  },
+  {
+    id: 8,
+    level: 8,
+    discount: 40,
+    img: '/elephant/8.png',
+    stars: 1200,
+    price: 800
+  },
+  {
+    id: 9,
+    level: 9,
+    discount: 45,
+    img: '/elephant/9.png',
+    stars: 1350,
+    price: 900
+  },
+  {
+    id: 10,
+    level: 10,
+    discount: 50,
+    img: '/elephant/9.png',
+    stars: 1500,
+    price: 1000
+  },
+
+])
 
 
   return (
@@ -29,18 +110,15 @@ export default function Buying() {
         modules={[Navigation]}
         navigation={true}
         className="mySwiper">
-        <SwiperSlide>
-          {' '}
-          <BuyingItem />
-        </SwiperSlide>
-        <SwiperSlide>
-          {' '}
-          <BuyingItem />
-        </SwiperSlide>
-        <SwiperSlide>
-          {' '}
-          <BuyingItem />
-        </SwiperSlide>
+          {data.map((item)=>(
+            <SwiperSlide>
+         
+          <BuyingItem item={item}/>
+         </SwiperSlide>
+          ))}
+        
+     
+       
       </Swiper>
     </section>
   );
--- src/components/pages/Buying/BuyingItem.jsx
+++ src/components/pages/Buying/BuyingItem.jsx
@@ -2,23 +2,25 @@ import React from 'react';
 import './buying.scss';
 import Button from '../../UI/Button/Button';
 
-export default function BuyingItem() {
+export default function BuyingItem({item}) {
+  console.log(item);
+  
  
   
 
   return (
     <div className="buying__item">
       <div className="buying__item_img">
-        <img src="/elephant/1.png" alt="elephant" />
+        <img src={item.img} alt="elephant" />
       </div>
       <div className="buying__item_info">
         <div className="buying__lvl">
           {/* <img className='custom-prev' src="/arrow-left.png" alt="<" /> */}
-          <span>Слон 1 lvl</span>
+          <span>Слон {item.level} lvl</span>
           {/* <img  className='custom-next' src="arrow-right.png" alt=">" /> */}
         </div>
         <div className="buying__discount">
-          <h4>5%</h4>
+          <h4>{item.discount}%</h4>
           <p>
             Вы зарабатываете с каждой <br /> продажи слона другу !
           </p>
@@ -27,14 +29,14 @@ export default function BuyingItem() {
           <h4>
             После покупки слона вашими друзьями  вы  получите{' '}
             <b >
-              150 
+              {item.stars} 
               <img src="/star.png" alt="star" />
             </b>
          
           </h4>
         </div>
         <div className='buying__btn'>
-        <Button className={'flex items-center'}>Купить <span className='ml-[48px]'>150</span> <img className='ml-[8px]' src="/white-star.png" alt="white-star" /></Button>
+        <Button className={'flex items-center'}>Купить <span className='ml-[48px]'>{item.price}</span> <img className='ml-[8px]' src="/white-star.png" alt="white-star" /></Button>
         </div>
       
       </div>
