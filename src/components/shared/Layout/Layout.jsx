49de6f6c0ad7372e9a5aeb965a42563cad988cbd
Baxa-1998 <baxtiyornarziev6@gmail.com>
2024-12-16 18:37:04 +0300

    ss

--- src/components/shared/Layout/Layout.jsx
+++ src/components/shared/Layout/Layout.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 import { Outlet } from 'react-router'
 import Header from '../Header/Header'
-
+import '../../../App.scss'
 export const Layout = () => {
   return (
-    <div className='max-w-[393px] m-auto'>
+    <div className='wrapper'>
       {/* <Header/> */}
       
       <main>
