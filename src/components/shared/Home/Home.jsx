06c0b02b5acddaf850cb4e2c5a4d93374e29cc1f
Baxa-1998 <baxtiyornarziev6@gmail.com>
2024-12-17 17:32:54 +0300

    remove click

--- src/components/shared/Home/Home.jsx
+++ src/components/shared/Home/Home.jsx
@@ -11,15 +11,15 @@ import Bought from '../../pages/Bought/Bought';
 export default function Home() {
   const [isElephant, setIsElephant] = React.useState(true);
   const { elephants, hasElephant } = useAppContext();
- useEffect(()=>{
-  console.log(elephants,hasElephant);
- },[elephants,hasElephant])
+  useEffect(() => {
+    console.log(elephants, hasElephant);
+  }, [elephants, hasElephant]);
 
   return (
     <section className="home">
       {/* home top section */}
       <div className="home__top">
-      {!hasElephant ?   <UserInfo /> : null}
+        {!hasElephant ? <UserInfo /> : null}
         {hasElephant ? (
           <Bought />
         ) : (
@@ -37,20 +37,27 @@ export default function Home() {
                   <Link to={'/buying'}>
                     <Button>Купить слона</Button>
                   </Link>
-                  <div className="flex items-center justify-center h-[65px] bg-[#262626] w-[95%] m-auto mt-[15px] rounded-[52px]">
+                  <div className="flex items-center justify-center h-[65px] bg-[#262626] w-[100%] m-auto mt-[15px] rounded-[52px]">
                     <h4 className="text-[#FEAC3E] font-bold text-[18px] text-center leading-6">
                       Зарабатывайте от <br /> 50% вместе со слоном !
                     </h4>
                   </div>
                   <div className="w-[95%] m-auto mt-[16px]">
                     <p className="text-[18px]">
-                      Продавайте слона, и зарабатывайте <br /> на друзьях которым его продали !
+                      Зарабатывайте до 70% <br />
+                      от стоимости слона!
                     </p>
-                    <p className="mt-[12px] text-[18px]">
-                      Получайте вознаграждения <br /> в виде{' '}
+                    <p className="mt-[20px] text-[18px]">
+                      Получайте вознаграждения <br /> в{' '}
                       <b className="text-[#FEAC3E]">
                         Telegram stars <img className="inline" src="/star.png" alt="" />
-                      </b>
+                      </b>{' '}
+                      с возможностью <br />
+                      вывода на карту
+                    </p>
+                    <p className='mt-[20px] text-[18px]'>
+                      Из-за повышенного спроса на <br /> слонов, полный функционал доступен только после
+                      покупки слона
                     </p>
                   </div>
                 </div>
