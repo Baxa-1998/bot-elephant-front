a546270bed1a945a1ce91d5e74cc881146496055
Baxa-1998 <baxtiyornarziev6@gmail.com>
2024-12-17 08:51:08 +0500

    new

--- src/components/shared/UserInfo/UserInfo.jsx
+++ src/components/shared/UserInfo/UserInfo.jsx
@@ -20,6 +20,7 @@ export default function UserInfo() {
       <div className="home__heading_left"> 
          {location.pathname === '/buying' &&      <img onClick={handleBack} src="/arrow-white.svg" alt="arrow-white" /> }
          {location.pathname === '/profile' &&      <img onClick={handleBack} src="/arrow-left.png" alt="arrow-white" /> }
+         {location.pathname === '/bought' &&      <img onClick={handleBack} src="/arrow-white.svg" alt="arrow-white" /> }
         <div className="avatar">
           <img src="/avatar1.png" alt="avatar" />
         </div>
