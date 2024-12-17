fb03463753b184a28702cbb69e166eed844be8ed
Baxa-1998 <baxtiyornarziev6@gmail.com>
2024-12-17 09:20:00 +0500

    new

--- src/components/pages/Bought/BoughtItem.jsx
+++ src/components/pages/Bought/BoughtItem.jsx
@@ -2,69 +2,57 @@ import React from 'react';
 import './bought.scss';
 import Button from '../../UI/Button/Button';
 import { Link } from 'react-router';
-import './bought.scss'
+import './bought.scss';
 export default function BoughtItem({ item }) {
-
-
   return (
     <div className="bought__item">
       <div className="bought__item_img">
         <img src={item.activeImg} alt="elephant" />
-
       </div>
-      <div className='bought__item_lvl'>
-        <h3>Слон</h3> 
+      <div className="bought__item_lvl">
+        <h3>Слон</h3>
         <p>{item.level} lvl</p>
-
       </div>
-      <div className='mt-[16px]'>
-        <Link to={'https://t.me/share/url?url={https://buy-elephant.netlify.app/}&text={Зарабатывай на слонах}'}>
-        <Button>Продать слона</Button>
+      <div className="mt-[16px]">
+        <Link
+          to={
+            'https://t.me/share/url?url={https://buy-elephant.netlify.app/}&text={Зарабатывай на слонах}'
+          }>
+          <Button>Продать слона</Button>
         </Link>
-
       </div>
-      
-      <div className='bought__prize'>
-          <h4>Вознаграждение</h4> 
-          <div className='bought__prize_item'> 
-            <h3>{item.stars}</h3>
-              <img src="/star.png" alt="" />
-          </div>
-          <div> 
 
-          </div>
+      <div className="bought__prize">
+        <h4>Вознаграждение</h4>
+        <div className="bought__prize_item">
+          <h3>{item.stars}</h3>
+          <img src="/star.png" alt="" />
+        </div>
+        <div></div>
       </div>
-      <div className='bought__task'>
-          <h4>Задача</h4> 
-          <div className='bought__task_item'> 
-           <p>Пригласить 5 друзей </p>
-           <p>0 / 5</p>
-        
-          </div>
-          <div> 
-
-          </div>
+      <div className="bought__task">
+        <h4>Задача</h4>
+        <div className="bought__task_item">
+          <p>Пригласить 5 друзей </p>
+          <p>0 / 5</p>
+        </div>
+        <div></div>
       </div>
-      <div className='w-full h-[1px] bg-[#CACACA] mt-[16px]'></div>
-      <div className='bought__advantage'>
-          <h4>Преимущества</h4> 
-          <div className='bought__advantage_item'> 
-            <h3>Бонус от продаж</h3>
-             <p>+ 50%</p>
-          </div>
-          <div> 
-
-          </div>
+      <div className="w-full h-[1px] bg-[#CACACA] mt-[16px]"></div>
+      <div className="bought__advantage">
+        <h4>Преимущества</h4>
+        <div className="bought__advantage_item">
+          <h3>Бонус от продаж</h3>
+          <p>+ 50%</p>
+        </div>
+        <div></div>
       </div>
 
       <div>
         <Link to={'/buying'}>
-        <Button className={'!text-[#000000] !rounded-lg mt-[20px]'}>Купить слона</Button>
+          <Button className={'!text-[#000000] !rounded-lg mt-[20px]'}>Купить слона</Button>
         </Link>
-      
       </div>
-   
-     
     </div>
   );
 }
