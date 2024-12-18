2d611b325c5dca9c7d4200f2207c91e95cae3a69
Baxa-1998 <baxtiyornarziev6@gmail.com>
2024-12-18 20:34:29 +0300

    s

--- src/components/pages/Bought/Bought.jsx
+++ src/components/pages/Bought/Bought.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './bought.scss';
 import UserInfo from '../../shared/UserInfo/UserInfo';
 import { useAppContext } from '../../../context/infoCTX';
@@ -9,23 +9,33 @@ import { Navigation } from 'swiper/modules';
 import BoughtItem from './BoughtItem';
 import BuyingItem from '../Buying/BuyingItem';
 import Buying from '../Buying/Buying';
+import Preloader from '../../shared/Preloader/Preloader';
 export default function Bought() {
   const { elephantsData } = useAppContext();
+  const [isLoading, setIsLoading] = React.useState(false);
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
 
-
-
-  return (
-    <section className="bought">
-      <div className="bought__top">
-        <UserInfo />
-      </div>
-      <Swiper modules={[Navigation]} navigation={true} className="mySwiper">
-        {elephantsData.map((item) => (
-          <SwiperSlide>
-            {item.purchased ? <BoughtItem item={item} /> : <BuyingItem item={item} />}
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </section>
-  );
+  if (isLoading) {
+    return <Preloader />;
+  } else {
+    return (
+      <section className="bought">
+        <div className="bought__top">
+          <UserInfo />
+        </div>
+        <Swiper modules={[Navigation]} navigation={true} className="mySwiper">
+          {elephantsData.map((item) => (
+            <SwiperSlide>
+              {item.purchased ? <BoughtItem item={item} /> : <BuyingItem item={item} />}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </section>
+    );
+  }
 }
